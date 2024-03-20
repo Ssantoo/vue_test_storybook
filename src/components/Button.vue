c88412edd30244a7aa0a473512d160fa9eb0e2d0
Ssantoo <nowing0108@kakao.com>
2024-03-20 18:34:45 +0900

    feat. storybook test

---
+++ src/components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-btn color="primary">{{ buttonText }}</v-btn>
+</template>
+
+<script>
+export default {
+    name: 'CustomButton',
+    props: {
+        buttonText: {
+            type: String,
+            default: 'test',
+        },
+    },
+}
+</script>
